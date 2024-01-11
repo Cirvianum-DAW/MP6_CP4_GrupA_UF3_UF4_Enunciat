d6bfb2a32a646cb672fbe5c34107ec5cd3205f2e
prosfp <110301233+prosfp@users.noreply.github.com>
2024-01-11 11:32:44 +0100

    canvis enunciat

---
+++ src/EX1/ex1.js
@@ -0,0 +1,16 @@
+// Funció GENÈRICA per fer la petició i obtenir dades d'una API amb gestió d'errors
+async function getData() {}
+
+// Funció per obtenir la informació desitjada dels personatges
+async function getCharacters() {}
+
+// Funció per mostrar els personatges
+function displayCharacters() {}
+
+// Funció per ordenar els personatges
+function sortCharacters() {}
+
+// Aquesta funció és llançarà automàticament quan la pàgina es carregui. Fes les crides necessàries per mostrar els personatges.
+window.addEventListener('DOMContentLoaded', async () => {
+  // També hauràs d'afegir l'event listener per quan es seleccioni un criteri d'ordenació i salti a la funció per mostrar  els personatges de manera ordenada
+});
